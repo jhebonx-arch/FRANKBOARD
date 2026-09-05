b60af86b8a6ebb43a70affed2508dda5596d606c
YourName <your-email@example.com>
2026-09-05 16:36:27 +0300

    Downgrade targetSdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.icecrusher.frankboard"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
